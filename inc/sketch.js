80d73db1780aa500dd7d47a0c458d2e64255efbf
Louis700 <louis.masson.contact@gmail.com>
2022-11-06 21:49:02 +0100

    :fire: Remove Block class

--- inc/sketch.js
+++ inc/sketch.js
@@ -1,14 +1,12 @@
 "use strict"
 
 let particles;
-let block;
 
 let windForce;
 
 function setup() {
 	windForce = new Vector(random(-0.05, 0.05), random(-0.0001, 0.0001));
 	particles = new Set();
-	block = new Block(new Vector(width/2 - 50, height/2 - 50), 100);
 
 	setInterval( createParticle, 10);
 	setInterval( () => windForce = new Vector(random(-0.05, 0.05), random(-0.0001, 0.0001)), 2000)
@@ -19,21 +17,11 @@ function setup() {
 function loop() {
 	background( new Color(200, 100, 10, 1, ColorType.HSL) );
 	
-	block.draw();
-
 	particles.forEach((particle) => {
-		if(block.collide(particle)) 
-			particle.lifeSpan = 0;	
-
-		if(particle.lifeSpan <= 0)
-			particles.delete(particle);
-
 		particle.update();
 		particle.draw();
 		particle.applyForce(new Vector(0, -0.1/particle.radius))
 		particle.applyForce(windForce);
-	
-
 	});
 }
 
@@ -44,6 +32,4 @@ function createParticle() {
 function clickListener(evt) {
 	let rect = evt.target.getBoundingClientRect();
 	let pos = new Vector(evt.clientX - rect.left, evt.clientY - rect.top);
-	
-	console.log(Vector.substract( pos, new Vector(block.position.x + block.side/2, block.position.y + block.side/2)).getAngle())
 }
